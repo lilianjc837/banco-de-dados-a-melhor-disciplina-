083945b3ba70ccaf9e810dd6a328dab56145c2bb
Lilian de Jesus Correia <102780880+lilianjc837@users.noreply.github.com>
2023-09-26 23:06:04 -0300

    exercício 3

--- ListaDeExercicios02.sql
+++ ListaDeExercicios02.sql
@@ -22,3 +22,17 @@ delimiter ;
 
 call sp_livros_por_categoria('Romance');
 call sp_livros_por_categoria('Ficção científica');
+
+-- 3
+delimiter //
+create procedure sp_contar_livros_por_categoria(in categoria_nome varchar(100), out total_livros int)
+begin
+    select count(*) into total_livros
+    from livro
+    inner join categoria on livro.categoria_id = categoria.categoria_id
+    where categoria.nome = categoria_nome;
+end;
+//
+delimiter ;
+call sp_contar_livros_por_categoria('História', @total);
+select @total;
