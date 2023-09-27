860a1a523e13d9ee68d09f04212633e72e875af9
Lilian de Jesus Correia <102780880+lilianjc837@users.noreply.github.com>
2023-09-26 23:05:40 -0300

    exercício 2

--- ListaDeExercicios02.sql
+++ ListaDeExercicios02.sql
@@ -7,3 +7,18 @@ end;
 DELIMITER ;
 
 call sp_ListarAutores();
+
+-- 2
+delimiter //
+create procedure sp_livros_por_categoria(in categoria_nome varchar(100))
+begin
+    select livro.titulo
+    from livro
+    inner join categoria on livro.categoria_id = categoria.categoria_id
+    where categoria.nome = categoria_nome;
+end;
+//
+delimiter ;
+
+call sp_livros_por_categoria('Romance');
+call sp_livros_por_categoria('Ficção científica');
