d2368e92f7802ff6244774f4e32a12e8bf72dcb4
Lilian de Jesus Correia <102780880+lilianjc837@users.noreply.github.com>
2023-09-26 23:09:25 -0300

    exercício 6

--- ListaDeExercicios02.sql
+++ ListaDeExercicios02.sql
@@ -71,3 +71,17 @@ delimiter ;
 
 call sp_verificar_livros_categoria('História', @possui_livros);
 select @possui_livros ;
+
+-- 6
+delimiter //
+create procedure sp_titulos_por_categoria(in categoria_nome varchar(100))
+begin
+    select livro.titulo
+    from livro
+    inner join categoria on livro.categoria_id = categoria.categoria_id
+    where categoria.nome = categoria_nome;
+end;
+//
+delimiter ;
+
+call sp_titulos_por_categoria('História');
